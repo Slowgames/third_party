200689a8a54c72de7086807482e040b4deea9127
chip collier <chip@slowgames.eu>
2024-12-12 09:22:17 +0100

    Renamed SDL_gpu_shadercross => SDL_shadercross

--- build_shadercross.sh
+++ build_shadercross.sh
@@ -3,16 +3,16 @@
 set -e
 
 cmake -B build/shadercross \
-      -S src/SDL_gpu_shadercross \
+      -S src/SDL_shadercross \
       -G 'Ninja Multi-Config' \
       -DCMAKE_DEBUG_POSTFIX="_debug" \
       -DCMAKE_PREFIX_PATH=`pwd`/dist/x86_64-linux \
       -DCMAKE_INSTALL_PREFIX=`pwd`/dist/shadercross/x86_64-linux \
-      -DSDLGPUSHADERCROSS_SPIRVCROSS_SHARED=OFF \
-      -DSDLGPUSHADERCROSS_VENDORED=ON \
-      -DSDLGPUSHADERCROSS_INSTALL=ON \
-      -DSDLGPUSHADERCROSS_SHARED=ON \
-      -DSDLGPUSHADERCROSS_INSTALL_RUNTIME=ON
+      -DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF \
+      -DSDLSHADERCROSS_VENDORED=ON \
+      -DSDLSHADERCROSS_INSTALL=ON \
+      -DSDLSHADERCROSS_SHARED=ON \
+      -DSDLSHADERCROSS_INSTALL_RUNTIME=ON
 
 # cmake --build build/shadercross --config Debug --target install
 
